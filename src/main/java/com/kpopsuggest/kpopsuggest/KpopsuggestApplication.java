46d5d333b79aab0d816d58ba309deb1fe198b691
Justin Artis <jusart1s8@gmail.com>
2020-07-18 21:08:06 -0400

    added aws and dynamodb basics

--- src/main/java/com/kpopsuggest/kpopsuggest/KpopsuggestApplication.java
+++ src/main/java/com/kpopsuggest/kpopsuggest/KpopsuggestApplication.java
@@ -2,8 +2,17 @@ package com.kpopsuggest.kpopsuggest;
 
 import Model.Artist;
 import Model.User;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.GetItemSpec;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.amazonaws.regions.Regions;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -23,12 +32,22 @@ public class KpopsuggestApplication {
 
 	@RequestMapping(value = "/Suggestions/{userId}",method = RequestMethod.GET, headers = "Accept=application/json")
 	public ArrayList<Artist> getUserSuggest(@PathVariable("userId") String userId) {
+		AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+				.withRegion(Regions.US_EAST_1)
+				.build();
+
+		DynamoDB dynamoDB = new DynamoDB(client);
+		Table songTable = dynamoDB.getTable("song_table");
+		GetItemSpec spec = new GetItemSpec().withPrimaryKey("songId", 1);
+		System.out.println(songTable.getItem(spec));
 		User user = new User();
 		user.setUsername(userId);
 		ArrayList artistList = new ArrayList();
 		if (isInputValid(user.getUsername())) {
+			//get user from database
 			return new ArrayList<>();
-		}else {
+		} else {
+			//Random suggestion selections from song table
 			Artist newArtist = new Artist();
 			newArtist.setName("Red Velvet");
 			newArtist.setLikes(10);
